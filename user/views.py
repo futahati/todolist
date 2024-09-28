b1273991c3634297542814ab21cbcad342caa568
irw <futahati@gmail.com>
2024-09-28 09:43:11 +0800

    完成註冊頁面修改

--- user/views.py
+++ user/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def index(request):
     message = ""
     form = UserCreationForm()
+    print(User.objects.all())
 
     if request.method == "POST":
         print(request.POST)
@@ -16,13 +18,17 @@ def index(request):
         # 比對密碼長度
         if len(password1) != 8 or len(password2) != 8:
             message = "密碼長度不正確！"
-
         # 比對密碼相同
         elif password1 != password2:
             message = "兩次密碼不一樣～"
-
-        # 比對使用都是否存在
-
-        # 註用使用者
+        else:
+            # 比對使用都是否存在
+            if User.objects.filter(username=username):
+                message = "帳號已存在！"
+            # 註用使用者
+            else:
+                user = User.objects.create_user(username=username, password=password1)
+                user.save()
+                message = "註冊成功。"
 
     return render(request, "user/register.html", {"form": form, "message": message})
