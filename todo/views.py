59eba124b992d4ed167d6b67b5e7b2cb44478f06
irw <futahati@gmail.com>
2024-10-05 10:55:04 +0800

    新增 todo.html

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,26 @@ from .models import Todo
 
 
 # Create your views here.
+def todo(request, id):
+    message = ""
+    user = request.user
+    todo = None
+    try:
+        todo = Todo.objects.get(id=id, user=user)
+
+    except Exception as e:
+        print(e)
+        message = "編號錯誤"
+
+    return render(request, "todo/todo.html", {"todo": todo, "message": message})
+
+
 def todolist(request):
-    todos = Todo.objects.all()
+    user = request.user
+
+    todos = None
+    if user.is_authenticated:
+        todos = Todo.objects.filter(user=user)
+
     print(todos)
     return render(request, "todo/todolist.html", {"todos": todos})
